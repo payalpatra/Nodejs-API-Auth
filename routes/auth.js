9846e98e3cd82031718e669b4547066bb2ad556c
payalpatra <payalpatra105@gmail.com>
2021-05-16 22:23:13 +0530

    Registration Validation Updated

--- routes/auth.js
+++ routes/auth.js
@@ -18,24 +18,26 @@ router.post("/register", async (req, res) => {
   // Validation of data before saving a user into the database
   const validation = validationSchema.validate(req.body);
   if (validation.error) return res.status(400).send(validation.error.details[0].message)
+  // Checking if the user already exists
+  const emailExists = await User.findOne({ email: req.body.email })
+  if (emailExists) return res.status(400).send("Email Already Exists")
 
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    try {
-      const savedUser = await user.save();
-      res.send("User Details Saved",savedUser);
-    } catch (err) {
-      res.status(400);
-    }
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400);
+  }
 
-  
 });
 
 router.post("/login", (req, res) => {
-  
+  res.send("Register");
 });
 
 module.exports = router;
